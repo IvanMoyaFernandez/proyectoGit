66fe9775ed3582d0ebb04c4f11b34c52866a1c7d
Ivan Moya <ivanmoya95@gmail.com>
2016-03-29 21:13:48 +0200

    hora pone: holaaa

--- Adios.java
+++ Adios.java
@@ -1,6 +1,6 @@
 public class Adios{
 	private static String nombre = "Ivan";
 	public static void main (String [] args){
-		System.out.println("Hola, " + nombre);
+		System.out.println("Holaaaa, " + nombre);
 	}
 }
