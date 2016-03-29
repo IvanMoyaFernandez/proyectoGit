1f98fe9a35338637d1856901691e7dbdad7ffd87
Ivan Moya <ivanmoya95@gmail.com>
2016-03-29 20:52:47 +0200

    Yo tambiÃn he hecho un commit, hello -> hola jiji

--- Adios.java
+++ Adios.java
@@ -1,6 +1,6 @@
 public class Adios{
 	private static String nombre = "Ivan";
 	public static void main (String [] args){
-		System.out.println("Hello, " + nombre);
+		System.out.println("Hola, " + nombre);
 	}
 }
